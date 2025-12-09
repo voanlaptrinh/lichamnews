02430d505cdaf6aca3e1aab1374b514a12afc450
khanh <voanlaptrinh@gmail.com>
2025-12-09 20:20:44 +0700

    ii

--- resources/views/la-so-tu-vi/results.blade.php
+++ resources/views/la-so-tu-vi/results.blade.php
@@ -164,16 +164,25 @@
                                     <div class="mb-3 ">
                                         <a href="{{ route('laso.download', ['url' => $imageUrl, 'ho_ten' => $normalizedData['ho_ten'] ?? '']) }}"
                                             class="btn btn-success me-2 mt-2 "
-                                            style="background: linear-gradient(45deg, #22ab50, #1f9643); border: none; padding: 10px 20px; border-radius: 25px; font-weight: 500;color: white; box-shadow: 0 4px 15px rgba(34, 84, 171, 0.3);"
+                                            style="background: linear-gradient(45deg, #2254AB, #4a90e2); border: none; padding: 10px 20px; border-radius: 25px; font-weight: 500;color: white; box-shadow: 0 4px 15px rgba(34, 84, 171, 0.3);"
                                             download>
-                                            <i class="fas fa-download"></i> Tải lá số
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-cloud-download" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383" />
+                                                <path
+                                                    d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708z" />
+                                            </svg> Tải lá số
                                         </a>
 
 
                                         <a href="{{ route('laso.create') }}"
-                                            style="background: linear-gradient(45deg, #22ab50, #1f9643);border: none;padding: 10px 20px; border-radius: 25px;font-weight: 500; color: white; box-shadow: 0 4px 15px rgba(34, 84, 171, 0.3);"
+                                            style="background: linear-gradient(45deg, #2254AB, #4a90e2);border: none;padding: 10px 20px; border-radius: 25px;font-weight: 500; color: white; box-shadow: 0 4px 15px rgba(34, 84, 171, 0.3);"
                                             class="btn btn-primary me-2  mt-2">
-                                            <i class="fas fa-plus"></i> Tạo lá số mới
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-plus" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5"/>
+  <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383m.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+</svg> Tạo lá số mới
                                         </a>
 
                                     </div>
@@ -475,7 +484,10 @@
                             </div>
                             <div class="luan-giai-overlay" id="luanGiaiOverlay">
                                 <button class="btn btn-primary xem-them-btn" onclick="showFullLuanGiai()">
-                                    <i class="fas fa-chevron-down"></i> Xem thêm
+                                     Xem thêm <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+</svg>
                                 </button>
                             </div>
                         </div>
